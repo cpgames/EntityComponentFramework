2d10cb64a093f200f9b6ea194c401b476d5f8851
cpgames <chillpillgames@gmail.com>
2024-10-06 14:52:55 -0500

    Adding long property

--- EntityComponentFramework/impl/properties/LongProperty.cs
+++ EntityComponentFramework/impl/properties/LongProperty.cs
@@ -1,10 +1,75 @@
-﻿namespace cpGames.core.EntityComponentFramework.impl
+﻿using System;
+using Newtonsoft.Json;
+
+namespace cpGames.core.EntityComponentFramework.impl
 {
-    public class LongProperty : Property<long>
+    public class LongPropertyConverter : JsonConverter
+    {
+        #region Methods
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            if (value == null ||
+                !((LongProperty)value).Get(out var val))
+            {
+                writer.WriteNull();
+                return;
+            }
+            writer.WriteValue(val);
+        }
+
+        public override object ReadJson(
+            JsonReader reader,
+            Type objectType,
+            object? existingValue,
+            JsonSerializer serializer)
+        {
+            if (existingValue is not LongProperty property)
+            {
+                throw new Exception("No existing value");
+            }
+            if (reader.Value == null)
+            {
+                property.Set(0);
+                return existingValue;
+            }
+            var longValue = Convert.ToInt64(reader.Value);
+            var setOutcome = property.SetData(longValue);
+            if (!setOutcome)
+            {
+                throw new Exception(setOutcome.ErrorMessage);
+            }
+            return existingValue;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            var isAssignable = objectType.IsAssignableFrom(typeof(LongProperty));
+            return isAssignable;
+        }
+        #endregion
+    }
+
+    [JsonConverter(typeof(LongPropertyConverter))]
+    public class LongProperty : Property<long>, ILongProperty
     {
         #region Constructors
-        public LongProperty(Entity owner, string name) : this(owner, name, 0) { }
-        public LongProperty(Entity owner, string name, long defaultValue) : base(owner, name, defaultValue) { }
+        public LongProperty(Entity owner, string name) : base(owner, name, 0) { }
+        #endregion
+
+        #region ILongProperty Members
+        public Outcome Add(long value)
+        {
+            return
+                Get(out var currentValue) &&
+                Set(currentValue + value);
+        }
+
+        public Outcome Subtract(long value)
+        {
+            return
+                Get(out var currentValue) &&
+                Set(currentValue - value);
+        }
         #endregion
     }
 }
